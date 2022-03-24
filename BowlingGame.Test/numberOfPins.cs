af961dd1bcf7e6956470a244e3fed337d6bb5ad7
jnamdegone <83634445+jnamdegone-yperia@users.noreply.github.com>
2022-03-24 10:27:59 +0100

    Test Pins

--- BowlingGame.Test/numberOfPins.cs
+++ BowlingGame.Test/numberOfPins.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BowlingGame;
+using System;
 
 namespace BowlingGame.Test
 {
@@ -12,23 +13,45 @@ namespace BowlingGame.Test
             int nbPins = 0;
             int expectedNbPins = 10;
 
-            Game game = new Game();
+            //Game game = new Game();
 
-            foreach(pins in game.Pins[])
+            for (int i = 0; i < 10; i++)
             {
                 nbPins++;
             }
+            Assert.AreEqual(expectedNbPins, nbPins, 10, "Nbpins is correctly");
 
-            if(nbPins > expectedNbPins)
-            {
-                Console.Write("Trop de quilles");
-            } else if(nbPins < expectedNbPins)
-            {
-                Console.Write("Pas assez de quilles");
-            } else
-            {
-                Console.Write("C'est bon");
-            }
+            //if (nbPins > expectedNbPins)
+            //{
+            //    Console.WriteLine("Trop de quilles");
+            //} else if(nbPins < expectedNbPins)
+            //{
+            //    Console.Writeline("Pas assez de quilles");
+            //} else
+            //{
+            //    Console.Writeline("C'est bon");
+            //}
+            //int nbPins = 0;
+            //int expectedNbPins = 10;
+
+            //Game game = new Game();
+
+            //foreach(pins in game.Pins[])
+            //{
+            //    nbPins++;
+            //}
+            //Assert.AreEqual(expectedNbPins, nbPins,10, "Nbpins not correctly");
+
+            //if (nbPins > expectedNbPins)
+            //{
+            //    Console.Writeline("Trop de quilles");
+            //} else if(nbPins < expectedNbPins)
+            //{
+            //    Console.Writeline("Pas assez de quilles");
+            //} else
+            //{
+            //    Console.Writeline("C'est bon");
+            //}
         }
     }
 }
